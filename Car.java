605f3799de51c43a0bf1f3dc4f70954d40ca1ca0
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-23 19:48:50 +0530

    added car program

---
+++ Car.java
@@ -0,0 +1,10 @@
+class Car {
+    
+        // Properties(Variables)
+        String color; 
+        int speed;
+
+        void drive() {
+            System.out.println("Car is runnning");
+        }
+}
