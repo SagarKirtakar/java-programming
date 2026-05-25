8e5d7d2ad6beafd738e607f33855c39828bca01e
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-25 16:47:35 +0530

    greatest of three number program

---
+++ Greatest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Greatest {
+    public static void main(String[] args) {
+        System.out.println("Enter any three number: ");
+         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a>b ) { // a = b
+            if(a>c) // a = c
+                System.out.println(a+" is greater");
+            else 
+                System.out.println(c+" is greater");
+        }else {
+            if(b>c) {   // b = c
+                System.out.println(b+" is greater");
+            }else{
+                System.out.println(c+" is greater");
+            }
+        }
+    }
+}
