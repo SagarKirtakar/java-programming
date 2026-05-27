49780268f270845db1dad7ba5553cab650860c38
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-28 05:26:59 +0530

    Factorial program 1.4

--- Factorial.java
+++ Factorial.java
@@ -7,12 +7,19 @@ public class Factorial {
     
     public static void main(String[] args) {
         
-        int n;
+        int n,fact=1;
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter any number: ");
 
         n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial "+fact);
+    
     }
 }
