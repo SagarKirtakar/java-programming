1702f55dee014f52f46b8d69249e26c1407a7c94
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-27 16:02:53 +0530

    Reverse number program 1.1

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,7 +1,17 @@
                   /*  Reverse Number Program */
 
+import java.util.Scanner;
+
 public class ReverseNumber {
     public static void main(String[] args) {
+
+        int r,n;
+
+        Scanner ref = new Scanner(System.in);
         
+        System.out.println("Enter any number: ");
+
+        n = ref.nextInt();
+
     }
 }
