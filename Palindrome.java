cc9dc1f74d3076ae5d802db2d3bfc6eaa102e953
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-28 04:38:48 +0530

    palindrome program 1.4

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,14 @@
             /*  w.a.p Palindrome number or not */
+// input number 121 ---> 121 
 
+import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter any number: ");
+
+        int num = sc.nextInt();
     }
 }
