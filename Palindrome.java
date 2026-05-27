7ee7d9d86e9d88e10f4a10c83e2e25e9b01672c3
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-28 04:49:40 +0530

    palindrome program 1.6

--- Palindrome.java
+++ Palindrome.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         
+        int n,s=0, r,c;
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Enter any number: ");
 
-        int n = sc.nextInt();
+        n = sc.nextInt();
 
-        int c=n;
+        c=n;
 
         while(n>0) {
-            
+           r = n%10;
+           s = (s*10) + r; 
         }
     }
 }
