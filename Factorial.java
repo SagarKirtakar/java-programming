d505884a2c072a9c11b309f74d9f8398ddf74179
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-28 05:11:13 +0530

    Factorial program 1.2

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,12 @@
         /*  W.a.p Factorial Number */
-// input n = 5 -----> 5 * 4 * 3 * 2 * 1
+// input n = 5 = 120 -----> 5 * 4 * 3 * 2 * 1
+
+import java.util.Scanner;
 
 public class Factorial {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
     }
 }
