4b6d475395de5738806380d10c796cb0d799c651
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-30 10:57:04 +0530

    Reverse String program 1.1

--- ReverseString.java
+++ ReverseString.java
@@ -3,14 +3,28 @@
 public class ReverseString {
     public static void main(String[] args) {
         
-        StringBuffer r = new StringBuffer("Learn Coding");
+        // StringBuffer r = new StringBuffer("Learn Coding");
        
-        System.out.println(r.reverse()); //gnidoC nraeL
+        // System.out.println(r.reverse()); //gnidoC nraeL
 
-        StringBuilder ref = new StringBuilder("Java Coding");
+        // StringBuilder ref = new StringBuilder("Java Coding");
         
-        System.out.println(ref.reverse()); // gnidoC avaJ
+        // System.out.println(ref.reverse()); // gnidoC avaJ
 
+        String s1 = "Learn Coding";
 
+        String s2 = "";
+
+        int len = s1.length();
+
+        for(int i = len-1; i>=0; i--) {
+
+            s2 = s2 + s1.charAt(i);
+        }
+
+        System.out.println("Original String: "+s1);
+
+        System.out.println("Reverse String: "+s2);
+        
     }
 }
