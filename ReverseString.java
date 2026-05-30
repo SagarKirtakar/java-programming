0af535bbbb984d150ac3e3ffcbf37b553884f943
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-30 10:48:49 +0530

    Reverse String program 1.0

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+            /* Reverse String Program  */
+
+public class ReverseString {
+    public static void main(String[] args) {
+        
+        StringBuffer r = new StringBuffer("Learn Coding");
+       
+        System.out.println(r.reverse()); //gnidoC nraeL
+
+        StringBuilder ref = new StringBuilder("Java Coding");
+        
+        System.out.println(ref.reverse()); // gnidoC avaJ
+
+
+    }
+}
