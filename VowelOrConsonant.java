6f5ed51d08eca3e03b1f5994eb9b5a0b415d1302
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-05-30 11:38:56 +0530

    Vowels or Consonant program 1.1

--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -1,8 +1,23 @@
                 /*  Check input characters are Vowel or Consonant */
 
+import java.util.Scanner;
+
 public class VowelOrConsonant {
     
     public static void main(String[] args) {
+        char ch;
+
+        Scanner ref = new Scanner(System.in);
         
+        System.out.println("Enter any character you want to check Vowels or Consotant: ");
+
+        ch = ref.next().charAt(0);
+
+        if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println("Vowels");
+        }else {
+            System.out.println("Consonant");
+        }
     }
 }
