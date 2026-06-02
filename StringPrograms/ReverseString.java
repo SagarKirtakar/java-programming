aa561e689a0bf3e26441517e263866b2bc9c04dd
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-02 16:01:38 +0530

    reverse String program 1.1

--- StringPrograms/ReverseString.java
+++ StringPrograms/ReverseString.java
@@ -1,5 +1,20 @@
+        /*  Reverse String  */
+
 package StringPrograms;
 
 public class ReverseString {
-    
+
+    public static void main(String[] args) {
+
+        String s = "Sagar";
+        String result = "";
+
+        int len = s.length();
+
+        for(int i = len - 1; i >= 0; i--) {
+            result = result + s.charAt(i);
+        }
+
+        System.out.println(result);
+    }
 }
