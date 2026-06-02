56383b2dce728e93f20c7a01c65b99d98a846826
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-02 20:11:05 +0530

    sort array without using inbuilt method

--- ArrayPrograms/ArraySort.java
+++ ArrayPrograms/ArraySort.java
@@ -1,5 +1,30 @@
 package ArrayPrograms;
 
+import java.util.Arrays;
+
 public class ArraySort {
-    
+    public static void main(String[] args) {
+        
+        int a[] = {7,5,3,1,8,2,3,6};
+
+        for(int i=0; i<a.length-1; i++) {
+
+            for(int j=0; j<a.length-1; j++) {
+
+                if(a[j] >  a[j+1]) {
+
+                    int temp = a[j];
+
+                    a[j] = a[j+1];
+
+                    a[j+1] = temp;
+
+                }
+            }
+        }
+
+        for(int num : a) {
+            System.out.println(num+" ");
+        }
+    }
 }
