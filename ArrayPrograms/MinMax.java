596113ef9a728c1ddb7f985f626d9ab3a691ed6f
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-02 19:48:27 +0530

    find min and max element on array

---
+++ ArrayPrograms/MinMax.java
@@ -0,0 +1,9 @@
+package ArrayPrograms;
+
+public class MinMax {
+    public static void main(String[] args) {
+        
+        int a[] = {10,7,8,99,65,22,70};
+        System.out.println(a[0]);
+    }
+}
