d860c9b2f54565766c30e5ca7320a8bd6a62218d
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-02 22:27:16 +0530

    remove duplicates from array

--- ArrayPrograms/DuplicateArray.java
+++ ArrayPrograms/DuplicateArray.java
@@ -3,5 +3,31 @@ package ArrayPrograms;
 public class DuplicateArray {
     public static void main(String[] args) {
         
+        int a[] = {7,3,5,1,3,4,1,6};
+
+        for(int i=0; i<a.length-1; i++) {
+            for(int j=1; j<a.length -1; j++) {
+                if(a[j] > a[j+1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+
+        int temp [] = new int[a.length];
+
+        int j=0;
+       for(int i=0; i<a.length-1; i++) {
+            if(a[i] != a[i+1]) {
+                temp[j] = a[i];
+                j++;
+            }
+       }
+
+       for(int num : temp) {
+        System.out.print(num);
+       }
+
     }
 }
